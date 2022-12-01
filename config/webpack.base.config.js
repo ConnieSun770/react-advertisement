db9fc4a0354e706c318f1e4b17ad220dddbac4ee
Gongyu Sun <gsun571@aucklanduni.ac.nz>
2022-12-01 13:43:42 +0800

    ts config

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,30 +1,34 @@
-const path = require('path')
+const path = require('path');
 
 const webpackBaseConfig = {
-    entry: path.join(__dirname,'../src/index.jsx'),
-    output:{
-        path:path.join(__dirname,'../dist'),
-        filename: "[name].[fullhash:4].js"
-    },
-    resolve: {
-        extensions: ['.js','.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test:/\.js[x]/,
-                use: 'babel-loader'
-            },
-            {
-                test:/\.(css|scss)/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    }
-}
+  entry: path.join(__dirname, '../src/index.jsx'),
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: '[name].[fullhash:4].js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js[x]/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.ts[x]/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.(css|scss)/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};
 
-module.exports = webpackBaseConfig
+module.exports = webpackBaseConfig;
--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,23 +1,22 @@
-const path = require('path')
-const {merge} = require('webpack-merge')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const webpackConfigBase = require('./webpack.base.config')
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
-
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpackConfigBase = require('./webpack.base.config');
 
 const webpackProdConfig = {
-    mode:'production',
-    plugins:[
-        new CleanWebpackPlugin({
-            protectWebpackAssets:true
-        }),
-        new HtmlWebpackPlugin({
-            inject:'body',
-            title:'React App',
-            filename:'index.html',
-            template:path.join(__dirname,'../src/index.html')
-        }),
-    ]
-}
+  mode: 'production',
+  plugins: [
+    new CleanWebpackPlugin({
+      protectWebpackAssets: true,
+    }),
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      title: 'React App',
+      filename: 'index.html',
+      template: path.join(__dirname, '../src/index.html'),
+    }),
+  ],
+};
 
-module.exports = merge(webpackProdConfig,webpackConfigBase)
+module.exports = merge(webpackProdConfig, webpackConfigBase);
