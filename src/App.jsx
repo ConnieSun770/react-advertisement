5d474a2dd17c5e6b64823dcd5a408878da324114
Gongyu Sun <gsun571@aucklanduni.ac.nz>
2022-12-01 13:01:26 +0800

    add eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React, {Component} from "react";
-import './style.scss'
+import React, { Component } from 'react';
+import './style.scss';
 
-class App extends Component{
-    render(){
-        return(
-            <div style={{color:'red'}}>
-                Hello World
-                <span className={'box'}>I am Connie.</span>
-            </div>
-        )
-    }
+class App extends Component {
+  render() {
+    return (
+      <div style={{ color: 'red' }}>
+        Hello World
+        <span className="box">I am Connie.</span>
+      </div>
+    );
+  }
 }
 
 export default App;
