4c84f270028250a7004d27abfd79a4cecc644aa5
Gongyu Sun <gsun571@aucklanduni.ac.nz>
2022-12-08 14:50:08 +0800

    add top up center

--- src/common/constants/topUp.ts
+++ src/common/constants/topUp.ts
@@ -2,14 +2,42 @@ const TOP_UP_LIST = [
   {
     id: 1,
     topUpValue: 60,
+    gift: 0,
   },
   {
     id: 2,
     topUpValue: 180,
+    gift: 16,
   },
   {
     id: 3,
     topUpValue: 300,
+    gift: 38,
+  },
+  {
+    id: 4,
+    topUpValue: 680,
+    gift: 88,
+  },
+  {
+    id: 5,
+    topUpValue: 1280,
+    gift: 138,
+  },
+  {
+    id: 6,
+    topUpValue: 1980,
+    gift: 268,
+  },
+  {
+    id: 7,
+    topUpValue: 3280,
+    gift: 568,
+  },
+  {
+    id: 8,
+    topUpValue: 6480,
+    gift: 1168,
   },
 ];
 
--- src/pages/topUpPage/index.tsx
+++ src/pages/topUpPage/index.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
+import TOP_UP_LIST from 'common/constants/topUp';
 import './styles.scss';
 import { connect } from 'react-redux';
 
@@ -29,8 +30,29 @@ class TopUpPage extends Component<IProps, IStates> {
         <div className="header-box">
           <Header history={history} username={name} />
         </div>
+        <div className="top-up-title">充值中心</div>
         <div className="content-box">
-
+          {
+            TOP_UP_LIST.map((topUpItem) => (
+              <div className="top-up-item-box">
+                <div className="top-content">
+                  <img className="icon" src="assets/imgs/card-icon1-selected.png" alt="icon" />
+                  {topUpItem.topUpValue}
+                </div>
+                {
+                  topUpItem.gift > 0 && <div className="tags">{`+${topUpItem.gift}`}</div>
+                }
+                <div className="bottom-content">
+                  <div className="price-circle">
+                    <div className="price">
+                      {topUpItem.topUpValue}
+                      <span className="unit">元</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))
+          }
         </div>
         <div className="footer-box">
           <Footer />
